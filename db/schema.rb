75a45f979df8807704bd454e4482b599240ab7d1
Warren Zhang <warrenzhangls@gmail.com>
2024-12-04 12:14:04 -0500

    created article files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_03_193236) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_04_165932) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_03_193236) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
