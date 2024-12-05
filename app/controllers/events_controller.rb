71152e2d260c8af64b844c66f9e8c76f7c1b46f9
Warren Zhang <warrenzhangls@gmail.com>
2024-12-04 23:16:36 -0500

    made events files

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,25 @@
 class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save
+    redirect_to event_path(@event)
+  end
+
+  private
+
+  def article_params
+    params.require(:event).permit(:user, :title, :location, :event_date, :number_of_people)
+  end
 end
