9c07f235f5e2bbbb7e06ee08934d4b2399671bed
Gerardo Arguelles <arguelles.gerardo.8@gmail.com>
2024-12-06 13:44:25 -0500

    Implemented bookmarks function, changed articles index, articles show seems broken

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_one_attached :profile_pic
   has_and_belongs_to_many :events
+  has_and_belongs_to_many :bookmarked_articles, class_name: 'Article', join_table: :bookmarks
 end
